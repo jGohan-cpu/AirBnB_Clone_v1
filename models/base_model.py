371bdaf2de76b5c018805bd7e0b835db6d8b7e30
jGohan-cpu <jjoseriveram01@gmail.com>
2023-07-03 10:39:07 -0400

    3 fixed

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
         '''
         self.id = str(uuid.uuid4)
         self.created_at = datetime.now()
-        self.updated_at = self.created.at
+        self.updated_at = self.created_at
 
     def __str__(self):
         '''
