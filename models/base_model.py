bf0631be2c36942bc1606744c3f01027951b565c
jGohan-cpu <jjoseriveram01@gmail.com>
2023-07-03 12:43:56 -0400

    4 exercise

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,21 @@ class BaseModel:
     BaseModel
     '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         construction function for objects
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            kwargs.pop('__class__', None)
+            self.__dict__.update(kwargs)
+            self.created_at = datetime.strptime(
+                self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(
+                self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         '''
