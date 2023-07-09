e8b896f1dc5ba2fc7e98d398967b9cff88530c29
jGohan-cpu <jjoseriveram01@gmail.com>
2023-07-09 01:26:33 -0400

    city adjusted

--- models/city.py
+++ models/city.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-""" City class that inherits from BaseModel"""
+"""
+City class that inherits from BaseModel
+"""
 
-BaseModel = models.base_model.BaseModel
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """City class
+    """
+    City class
     Attributes:
         state_id (str): state identifier
         name (str): name of the city
     """
     state_id = ""
     name = ""
-
-    def __init__(self, state_id=None, name=None):
-        self.state_id = state_id
-        self.name = name
-
-    @property
-    def state(self):
-        return (self.name + '(' + self.state_id + ')')
