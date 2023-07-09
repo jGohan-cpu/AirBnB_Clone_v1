c139aacb73e88c1b8dd45274ab49ad3b80e44c96
jGohan-cpu <jjoseriveram01@gmail.com>
2023-07-09 01:26:54 -0400

    place adjusted

--- models/place.py
+++ models/place.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Place class that inherits from BaseModel"""
 
-BaseModel = models.base_model.BaseModel
+"""
+Place class that inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Place class
     """
-
-    city_id = 0
+    Place class
+    """
+    city_id = ""
     user_id = ""
     name = ""
     description = ""
@@ -16,31 +19,6 @@ class Place(BaseModel):
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
-    latitude = 0
-    longitude = 0
-    amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        """ Attributes:
-        city_id (str): city identifier
-
-        user_id (str): user identifier
-
-        name (str): name of place
-
-        description (str): description of place
-
-        number_rooms (int): number of rooms
-
-        number_bathrooms (int): number of bathrooms
-
-        max_guest (int): maximum guest
-
-        price_by_night (int): price of stay
-
-        latitude (float): latitude
-
-        longitude (float): longitude
-
-        amenity_ids (str): list of amenities ids"""
-        pass
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = [""]
